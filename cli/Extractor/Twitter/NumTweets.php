db1d48922d4b1b916ce4f4e86586fb952b4679c9
Rafael Andreatta <eu@rafaa.me>
2016-09-23 15:06:05 -0300

    Adding various twitter and a google feature extractor

---
+++ cli/Extractor/Twitter/NumTweets.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Copyright (c) 2012-2016 Veridu Ltd <https://veridu.com>
+ * All rights reserved.
+ */
+
+declare(strict_types = 1);
+
+namespace Cli\Extractor\Twitter;
+
+use Cli\Extractor\AbstractExtractor;
+
+class NumTweets extends AbstractExtractor {
+    /**
+     * {@inheritdoc}
+     */
+    public function execute() {
+        $profile = $this->worker->rawBuffer->getData('profile');
+
+        if (empty($profile['statuses_count'])) {
+            return 0;
+        }
+
+        return $profile['statuses_count'];
+    }
+}
