6679256c74b2457d823703dadead51b9bd701259
Rafael Andreatta <eu@rafaa.me>
2016-09-26 14:23:35 -0300

    Added various feature extractors for spotify

---
+++ cli/Extractor/Spotify/CurrentCountryName.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Copyright (c) 2012-2016 Veridu Ltd <https://veridu.com>
+ * All rights reserved.
+ */
+
+declare(strict_types = 1);
+
+namespace Cli\Extractor\Spotify;
+
+use Cli\Extractor\AbstractExtractor;
+
+class CurrentCountryName extends AbstractExtractor {
+    /**
+     * {@inheritdoc}
+     */
+    public function execute() {
+        $profile = $this->worker->rawBuffer->getData('profile');
+
+        if (empty($profile['country'])) {
+            return;
+        }
+
+        //@FIXME
+        //Utils::getInstance()->codeToCountry()
+        return;
+    }
+}
---
+++ cli/Extractor/Spotify/IsCelebrityName.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Copyright (c) 2012-2016 Veridu Ltd <https://veridu.com>
+ * All rights reserved.
+ */
+
+declare(strict_types = 1);
+
+namespace Cli\Extractor\Spotify;
+
+use Cli\Extractor\AbstractExtractor;
+
+class IsCelebrityName extends AbstractExtractor {
+    /**
+     * {@inheritdoc}
+     */
+    public function execute() {
+        $fullName = $this->worker->parsedBuffer->waitData('fullName');
+
+        if (empty($fullName)) {
+            return;
+        }
+
+        //@FIXME
+        //return Utils::getInstance()->isCelebrityName($fullName);
+        return;
+    }
+}
